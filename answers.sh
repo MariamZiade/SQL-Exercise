d5cce831132a5e789cd27d914daf7b457c0f151a
MariamZiade <mariam.ziade@gmail.com>
2021-03-21 15:37:33 +0200

    Answers and  myDatabase updated

---
+++ answers.sh
@@ -0,0 +1,9 @@
+Basic Queries
+"SELECT * FROM students"
+"SELECT Name FROM students"
+"SELECT * FROM students WHERE Age > 30"
+"SELECT Name FROM students WHERE Age = 30 AND Gender = "F""
+"SELECT Points FROM students WHERE name = "Alex""
+"INSERT INTO students VALUES (8, "Mariam", 31, "F", 500)"
+"UPDATE students SET Points = Points + 100 WHERE name = "Basma""
+"UPDATE students SET points = points - 10 WHERE name = "Alex""
